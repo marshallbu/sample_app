9cdf9ec0ad4f0ce250d3c41a435e0b728c6e32d7
Marshall Upshur <marshall@mbucreations.com>
2012-04-28 22:20:13 -0700

    changed Pages to StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+  get "static_pages/home"
 
-  get "pages/contact"
+  get "static_pages/contact"
   
-  get "pages/about"
+  get "static_pages/about"
   
-  get "pages/help"
+  get "static_pages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe PagesController do
+describe StaticPagesController do
   render_views
   
   before(:each) do
